0dc9a18433bc043009ac9a39885eef547bc624a3
maartenbreddels <maartenbreddels@gmail.com>
2016-10-11 16:55:14 +0200

    first attempt at docker

---
+++ build_linux.sh
@@ -0,0 +1,24 @@
+set -x
+ls /opt/python/
+export PATH=/opt/python/3.5/bin:$PATH
+which python
+
+#pip install
+
+git clone https://github.com/maartenbreddels/vaex
+cd vaex
+pip install --upgrade pip
+pip install --upgrade matplotlib
+FILE=`python -c "import matplotlib.backends.backend_tkagg as m; print (m.__file__)"`;
+rm -f $FILE ${FILE}c;
+pip install numpy
+pip install -r requirements.txt
+pip install -e .
+pip install PyQt5
+pip install qtconsole appdirs
+pip install setuptools==19.2
+pip install requests==2.5.1
+pip install pyinstaller==3.1
+cp misc/vaex.spec .
+export MPLBACKEND=qt5agg
+pyinstaller --hidden-import requests.auth vaex.spec
---
+++ build_osx.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+brew update;
+brew install python;
+brew install brew install pyqt5 --with-python --without-python3;
+pip install virtualenv;
+virtualenv venv --no-site-packages;
+source venv/bin/activate;
+pip install --upgrade pip;
+pip install --upgrade matplotlib;
+FILE=`python -c "import matplotlib.backends.backend_tkagg as m; print (m.__file__)"`;
+rm -f $FILE ${FILE}c;
+pip install numpy;
+pip install -r vaex/requirements.txt;
+pip install -e vaex;
+pip install qtconsole appdirs;
+pip install setuptools==19.2;
+pip install requests==2.5.1;
+
+pip install macholib==1.5.1 modulegraph==0.11 py2app==0.7
+cd vaex
+python setup.py install
+python setup.py py2app
+cd ..
