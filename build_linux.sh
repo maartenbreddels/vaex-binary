9eaaa49c7dcbbf33d78afe21b8d083012224bd77
maartenbreddels <maartenbreddels@gmail.com>
2016-10-11 17:05:17 +0200

    chmod +x scripts

--- build_linux.sh
+++ build_linux.sh
@@ -19,6 +19,8 @@ pip install qtconsole appdirs
 pip install setuptools==19.2
 pip install requests==2.5.1
 pip install pyinstaller==3.1
+cd vaex
 cp misc/vaex.spec .
 export MPLBACKEND=qt5agg
 pyinstaller --hidden-import requests.auth vaex.spec
+cd ..
