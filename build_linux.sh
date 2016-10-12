03cb67bcdf1758d8adbf3ae26fb9d285feebae39
maartenbreddels <maartenbreddels@gmail.com>
2016-10-12 22:33:05 +0200

    cleanups and actually use the virtualenv

--- build_linux.sh
+++ build_linux.sh
@@ -3,16 +3,16 @@ set +x
 
 pip install virtualenv;
 virtualenv venv --no-site-packages;
-virtualenv venv --no-site-packages;
+source venv/bin/activate;
 pip install requests[security];
-git clone https://github.com/maartenbreddels/vaex
 
-cd vaex
 pip install --upgrade pip
 pip install --upgrade matplotlib
-
 FILE=`python -c "import matplotlib.backends.backend_tkagg as m; print (m.__file__)"`;
 rm -f $FILE ${FILE}c;
+
+git clone https://github.com/maartenbreddels/vaex
+cd vaex
 
 pip install numpy # kapteyn package complaints otherwise
 pip install -r requirements.txt
@@ -22,15 +22,7 @@ pip install qtconsole appdirs
 pip install setuptools==19.2
 pip install requests==2.5.1
 
-#pip install --upgrade pip
-#pip install --upgrade matplotlib
-FILE=`python -c "import matplotlib.backends.backend_tkagg as m; print (m.__file__)"`;
-rm -f $FILE ${FILE}c;
-pip install qtconsole appdirs
-pip install setuptools==19.2
-pip install requests==2.5.1
 pip install pyinstaller==3.1
-pip install appdirs
 cp misc/vaex.spec .
 export MPLBACKEND=qt5agg
 pyinstaller --hidden-import requests.auth vaex.spec
