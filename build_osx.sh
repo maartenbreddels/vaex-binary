49cec1b34d961c7f12770b7e1b48d1f43d0c8f61
maartenbreddels <maartenbreddels@gmail.com>
2016-10-12 16:49:36 +0200

    put miniconda in PATH

--- build_osx.sh
+++ build_osx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 curl http://vaex.astro.rug.nl/install_conda.sh | bash -s -- --dev --python 2
+export PATH=$HOME/miniconda-vaex/bin:$PATH
 pip install py2app
 cd vaex
 python setup.py install
