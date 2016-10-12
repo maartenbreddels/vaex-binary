07d23cd84bd36d6d8e801440ddc6fd0127c36aa8
maartenbreddels <maartenbreddels@gmail.com>
2016-10-12 22:40:05 +0200

    use pyqt4 from install script directly

--- build_osx.sh
+++ build_osx.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-curl http://vaex.astro.rug.nl/install_conda.sh | bash -s -- --dev --python 2
+curl http://vaex.astro.rug.nl/install_conda.sh | bash -s -- --dev --python 2 --pyqt 4
 export PATH=$HOME/miniconda-vaex/bin:$PATH
-conda install pyqt=4
 pip install py2app
 cd vaex
 python setup.py install
