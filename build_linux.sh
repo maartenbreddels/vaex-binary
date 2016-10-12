c67b4e8a19e8cb2ef1067af3f23a1010591241b0
maartenbreddels <maartenbreddels@gmail.com>
2016-10-12 15:51:01 +0200

    and back we got to normal python3.5 for linux, python2.7/conda for osx

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+
+pip install virtualenv;
+virtualenv venv --no-site-packages;
+virtualenv venv --no-site-packages;
+pip install requests[security];
+git clone https://github.com/maartenbreddels/vaex
+
+cd vaex
+pip install --upgrade pip
+pip install --upgrade matplotlib
+
+FILE=`python -c "import matplotlib.backends.backend_tkagg as m; print (m.__file__)"`;
+rm -f $FILE ${FILE}c;
+
+pip install numpy # kapteyn package complaints otherwise
+pip install -r requirements.txt
+pip install -e .
+pip install PyQt5
+pip install qtconsole appdirs
+pip install setuptools==19.2
+pip install requests==2.5.1
+
 #pip install --upgrade pip
 #pip install --upgrade matplotlib
 FILE=`python -c "import matplotlib.backends.backend_tkagg as m; print (m.__file__)"`;
@@ -8,6 +30,7 @@ pip install qtconsole appdirs
 pip install setuptools==19.2
 pip install requests==2.5.1
 pip install pyinstaller==3.1
+pip install appdirs
 cd vaex
 cp misc/vaex.spec .
 export MPLBACKEND=qt5agg
--- build_osx.sh
+++ build_osx.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+curl http://vaex.astro.rug.nl/install_conda.sh | bash -s -- --dev --python 2
 pip install py2app
 cd vaex
-#python setup.py install
+python setup.py install
 python setup.py py2app
 cd ..
